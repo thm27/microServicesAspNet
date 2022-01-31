43ceb46a67a462d1660e6aa34aa20df52738fb93
thm27 <40831358+thm27@users.noreply.github.com>
2022-01-31 14:03:04 -0300

    Discount.GRPC communicating with Basket.API, docker compose file updated.

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -1,4 +1,6 @@
+using Basket.API.GRPCServices;
 using Basket.API.Repositories;
+using Discount.GRPC.Protos;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -32,6 +34,12 @@ namespace Basket.API
             });
 
             services.AddScoped<IBasketRepository, BasketRepository>();
+
+            services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
+                (o => o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
+            
+            services.AddScoped<DiscountGrpcService>();
+
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
